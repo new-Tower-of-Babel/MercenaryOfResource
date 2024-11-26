3389c00088c84a9cbcdb682bca4aabf731433bdf
trillionisjo <trillionisjo@gmail.com>
2024-11-26 10:34:31 +0900

    Fix : 플레이어 이동 시 떨림 현상 고침

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,10 +6,13 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float rotateSpeed = 10f;
+    [SerializeField] private float fireDelay = 0.3f;
+    [SerializeField] private GameObject projectilePrefab;
 
     private Rigidbody rb;
 
     private Vector3 aimDir;
+    private float fireDelayCounter = 0f;
 
     private void Awake()
     {
@@ -21,21 +24,29 @@ public class Player : MonoBehaviour
         GameInput.Instance.OnAimAction += GameInput_OnAimAction;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
+        // move
         Vector2 inputVector = GameInput.Instance.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3 (inputVector.x, 0f, inputVector.y);
         rb.velocity = moveDir * moveSpeed;
-        //transform.position += moveDir * moveSpeed * Time.deltaTime
-
-        Vector3 dir = GameInput.Instance.IsAiming ? aimDir : moveDir; 
+        
+        // turn
+        Vector3 dir = GameInput.Instance.IsAiming ? aimDir : moveDir;
         transform.forward = Vector3.Slerp (transform.forward, dir, Time.deltaTime * rotateSpeed);
+    }
+
+    private void Update()
+    {
+        if (GameInput.Instance.IsAiming)
+        {
+            
+        }
     }
     
     private void GameInput_OnAimAction (Vector3 mouseWorldPos)
     {
         aimDir = mouseWorldPos - transform.position;
-        Debug.Log (mouseWorldPos);
     }
 
 }
