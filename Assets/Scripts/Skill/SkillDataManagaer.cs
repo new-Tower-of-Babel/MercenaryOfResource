9a90cdc01ad79706fc5d89eae230420cb92706f2
new-Tower-of-Babel <net6994@gmail.com>
2024-11-29 20:30:44 +0900

    [Add]SkillDataManager

---
+++ Assets/Scripts/Skill/SkillDataManagaer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillDataManagaer : SingletonBase<SkillDataManagaer>
+{
+    public static int skill_MoveSpeed_Level = 0;
+    public static int skill_CarryQuantity_Level = 0;
+    public static int skill_collectPower_Level = 0;
+    public static int skill_CutlogSpeed_Level = 0;
+    public static int skill_MiningSpeed_Level = 0;
+
+    public static int skill_AttackPower_Level = 0;
+    public static int skill_HP_Level = 0;
+    public static int skill_DefensePower_Level = 0;
+    public static int skill_AttackSpeed_Level = 0;
+
+    public static int skill_Grenade_Level = 0;
+    public static int skill_Mine_Level = 0;
+    
+}
