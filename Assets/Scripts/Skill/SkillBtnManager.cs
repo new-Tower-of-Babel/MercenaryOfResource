69858d1399cf9119c92e50363e8163b89d94e1d2
new-Tower-of-Babel <net6994@gmail.com>
2024-12-09 10:39:10 +0900

    [Update]Skillsystem
    스킬시스템 구현중

--- Assets/Scripts/Skill/SkillBtnManager.cs
+++ Assets/Scripts/Skill/SkillBtnManager.cs
@@ -12,17 +12,27 @@ public class SkillBtnManager : MonoBehaviour
 
     private Button selectedBtn;
 
+    [SerializeField] GameObject upgradeBtn;
+
     void Start()
     {
+        upgradeBtn.SetActive(false);
         foreach (Button btn in SkillBtn)
         {
             btn.onClick.AddListener(() => OnSkillButtonClicked(btn));
         }
+    }
+    private string ToLowerFirstChar(string input) //첫글자 소문자로 변환
+    {
+        if (string.IsNullOrEmpty(input))
+            return input;
 
+        return char.ToLower(input[0]) + input.Substring(1);
     }
 
-    void OnSkillButtonClicked(Button clickedButton)
+    void OnSkillButtonClicked(Button clickedButton)  //버튼선택
     {
+        
         if (selectedBtn != null)
         {
             selectedBtn.GetComponent<Image>().color = Color.white;
@@ -31,10 +41,33 @@ public class SkillBtnManager : MonoBehaviour
         clickedButton.GetComponent<Image>().color = Color.green;
         selectedBtn = clickedButton;
 
-        string skillInfo = clickedButton.GetComponentInChildren<TextMeshProUGUI>().text; // 예시
-        SkillDataManagaer.currentSkill = clickedButton.ToString();
-        Debug.Log(SkillDataManagaer.currentSkill);
+        string btnName = clickedButton.name;
+        string skillName = ToLowerFirstChar(btnName);
+        UpgradeCheck(skillName);
+        SkillDataManagaer.currentSkill = skillName;
+
+        string skillInfo = clickedButton.GetComponentInChildren<TextMeshProUGUI>().text;
+        
         skillInfoText.text = skillInfo;
         skillInfoPanel.SetActive(true);
+    }
+
+    void UpgradeCheck(string skillName)
+    {
+        if (!SkillDataManagaer.Skills[skillName])
+        {
+            upgradeBtn.SetActive(true);
+        }
+        else
+        {
+            upgradeBtn.SetActive(false);
+        }
+    }
+
+    public void UpgradeBtn()
+    {
+        //todo.소유한 자본에 따라서 가능하게끔 변경할것 if
+        SkillDataManagaer.Skills[SkillDataManagaer.currentSkill] = true;
+        upgradeBtn.SetActive(false);
     }
 }
--- Assets/Scripts/Skill/SkillDataManagaer.cs
+++ Assets/Scripts/Skill/SkillDataManagaer.cs
@@ -2,21 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SkillDataManagaer : SingletonBase<SkillDataManagaer>
+public class SkillDataManagaer : MonoBehaviour
 {
-    public static int skill_MoveSpeed_Level = 0;
-    public static int skill_CarryQuantity_Level = 0;
-    public static int skill_collectPower_Level = 0;
-    public static int skill_CutlogSpeed_Level = 0;
-    public static int skill_MiningSpeed_Level = 0;
-
-    public static int skill_AttackPower_Level = 0;
-    public static int skill_HP_Level = 0;
-    public static int skill_DefensePower_Level = 0;
-    public static int skill_AttackSpeed_Level = 0;
-
-    public static int skill_Grenade_Level = 0;
-    public static int skill_Mine_Level = 0;
+    public static Dictionary<string, bool> Skills = new Dictionary<string, bool>
+    {
+        { "skill_TreeA_MoveSpeed_Level_1", false },
+        { "skill_TreeA_MoveSpeed_Level_2", false },
+        { "skill_TreeA_MoveSpeed_Level_3", false },
+        { "skill_TreeA_CarryQuantity_Level_1", false },
+        { "skill_TreeA_CarryQuantity_Level_2", false },
+        { "skill_TreeB_CollectPower_Level_1", false },
+        { "skill_TreeB_CollectPower_Level_2", false },
+        { "skill_TreeB_CollectPower_Level_3", false },
+        { "skill_TreeB_CutlogSpeed_Level_1", false },
+        { "skill_TreeB_CutlogSpeed_Level_2", false },
+        { "skill_TreeB_MiningSpeed_Level_1", false },
+        { "skill_TreeB_MiningSpeed_Level_2", false },
+        { "skill_AttackPower_Level", false },
+        { "skill_HP_Level", false },
+        { "skill_DefensePower_Level", false },
+        { "skill_AttackSpeed_Level", false },
+        { "skill_Grenade_Level", false },
+        { "skill_Mine_Level", false }
+    };
 
     public static string currentSkill;
 }
