443c9af9cb42bdad6c7d3e030f5cbad6dd458c2f
new-Tower-of-Babel <net6994@gmail.com>
2024-12-12 14:45:26 +0900

    [FIx]SkillTreeBMiningSpeed2
    잘못된 부분 수정

--- Assets/Scripts/Skill/SkillList/SkillTreeBMiningSpeed2.cs
+++ Assets/Scripts/Skill/SkillList/SkillTreeBMiningSpeed2.cs
@@ -4,5 +4,5 @@ public class SkillTreeBMiningSpeed2 : MonoBehaviour, ISkill
 {
     public int SkillKey { get; }  = 2301;
     public Dictionary<int,bool> AntecedentSkills { get; } = 
-        new Dictionary<int,bool> {{ 2102,false }}; //SkillTreeBCollectPower2
+        new Dictionary<int,bool> {{ 2302,false }}; //SkillTreeBCollectPower2
 }
