c8abaa0e6ceb446dddc662a61aa9c919f8b015f5
ojs0124 <ojs_0124_@naver.com>
2025-01-13 20:42:24 +0900

    audio

--- Assets/Scripts/Manager/AudioManager.cs
+++ Assets/Scripts/Manager/AudioManager.cs
@@ -8,22 +8,36 @@ public class AudioManager : SingletonBase<AudioManager>
     private AudioSource _bgmSource;
 
     [Header("BGM")]
-    [SerializeField] private AudioClip bgmClip;
+    [SerializeField] private AudioClip bgm;
 
     [Header("SFX")]
-    [SerializeField] private AudioClip fireSFX;
+    [SerializeField] private AudioClip dayChange;
+    [SerializeField] private AudioClip fire;
+
+    [Header("UI")]
+    [SerializeField] private AudioClip click;
+
+    public float globalVolume = 50;
+    public float bgmVolume = 50;
+    public float sfxVolume = 50;
+    public float uiVolume = 50;
 
     private Dictionary<string, ObjectPool> _audioPools;
 
     public override void Awake()
     {
+        base.Awake();
         _audioPools = new Dictionary<string, ObjectPool>();
+        SetBGMSource();
     }
 
     public override void Start()
     {
-        SetBGMSource();
-        InitializeAudioPool(fireSFX, 10);
+        if (fire != null) InitializeAudioPool(fire, 10);
+        else Debug.LogWarning("Fire SFX clip is missing!");
+
+        if (click != null) InitializeAudioPool(click, 5);
+        else Debug.LogWarning("Click SFX clip is missing!");
     }
 
     // 오디오 풀을 초기화하고 딕셔너리에 추가
@@ -42,7 +56,9 @@ public class AudioManager : SingletonBase<AudioManager>
 
         // Set bgm property
         _bgmSource.loop = true;
-        _bgmSource.volume = 0.15f;
+
+        // Set volume
+        _bgmSource.volume = globalVolume * (bgmVolume / 100f);
     }
 
     public void PlayBGM(AudioClip clip)
@@ -53,6 +69,8 @@ public class AudioManager : SingletonBase<AudioManager>
             _bgmSource.clip = clip;
             _bgmSource.Play();
         }
+
+        _bgmSource.volume = globalVolume * (bgmVolume / 100f);
     }
 
     // SFX 사운드를 지정된 풀에서 재생
@@ -65,6 +83,8 @@ public class AudioManager : SingletonBase<AudioManager>
             ObjectPool pool = _audioPools[poolName];
             GameObject audioSourceObject = pool.GetObject();
             AudioSource audioSource = audioSourceObject.GetComponent<AudioSource>();
+
+            audioSource.volume = globalVolume * (sfxVolume / 100f);
             audioSource.PlayOneShot(clip);
 
             StartCoroutine(ReturnAudioSourceAfterPlay(audioSourceObject, poolName));
@@ -77,7 +97,4 @@ public class AudioManager : SingletonBase<AudioManager>
         yield return new WaitForSeconds(audioSource.clip.length);
         _audioPools[poolName].ReturnObject(audioSourceObject);
     }
-
-    // examples
-    public void PlayFireSFX() => PlaySFX(fireSFX);
 }
